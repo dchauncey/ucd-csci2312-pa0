333b0f67c3f3660be2a9be9acb0b5a03aa047ef3
dchauncey <dakota.chauncey@ucdenver.edu>
2016-01-20 12:29:54 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 /**
  * File: main.cpp
  * Project: ucd-csci2312-pa0
- * Author: Ivo Georgiev
- * Date: 2016-01-18
+ * Author: Dakota Chauncey
+ * Date: 2016-01-20
  */
 #include <iostream>
 
 int main() {
-    // TODO Substitute 11 with your UCD ID number, modulo 13 (myId % 13)
-    std::cout << "Hello, world! My number is " << 11 << '!' << std::endl;
+    int myID = 105951816
+    std::cout << "Hello, world! My number is " << myID %13 << '!' << std::endl;
 
     return 0;
 }
